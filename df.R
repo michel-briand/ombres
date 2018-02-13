be69f1dc00550a4418acf20df58f6c591fd0f782
Michel Briand <michelbriand@free.fr>
2018-02-13 11:40:30 +0100

    quelques tests rotation photos couchées

--- df.R
+++ df.R
@@ -11,14 +11,14 @@ df <- data.frame(model_names, model_width, model_height, model_model)
 photo_debout <- "/home/michel/PHOTOS/Charlotte/ur/famille/Les loulous/2014/fevrier/20140222_201352.jpg"
 photo_couchee <- "/home/michel/PHOTOS/Charlotte/ur/famille/Les loulous/2014/aout/20140822_161707.jpg"
 library(exifr)
-ec <- read_exif(photo_couchee)
+system.time( ec <- read_exif(photo_couchee) )
 print(ec["Orientation"])
 ed <- read_exif(photo_debout)
 print(ed["Orientation"])
 
 library(imager)
-ic <- load.image(photo_couchee)
-plot(im)
+ir <- ic <- load.image(photo_couchee)
+#plot(ic)
 
 o <- ec["Orientation"][[1]]
 switch(o,
@@ -28,9 +28,45 @@ switch(o,
   { print("4: orientation mirroir vertical F tête en bas") },
   { print("5: orientation mirroir vertical couchée F pointe à gauche regardant vers le bas") },
   { print("6: orientation couchée F regardant vers le haut")
-    ir <- imrotate(ic) },
+    ir <- imrotate(ic, angle = 90) },
   { print("7: orientation miroir vertical couchée F pointe à droite regardant vers le haut") },
   { print("8: orientation couchée F regardant vers le bas")}
 )
 
+#ir <- imrotate(ic)
 plot(ir)
+dim(ic)
+
+library(jpeg)
+jc <- readJPEG(photo_couchee)
+plot(1:2, type='n')
+rasterImage(jc, 1, 1.25, 1.1, 1)
+
+library(magick)
+str(magick::magick_config())
+ic_ <- image_read(photo_couchee)
+image_info(ic_)
+plot(ic_)
+image_rotate(ic_, 90) %>% plot()
+
+library(imager)
+library(purrr)
+parrots <- load.example("parrots")
+plot(parrots)
+#Define a function that converts to YUV, blurs a specific channel, and converts back
+bchan <- function(im,ind,sigma=5) { 
+  im <- RGBtoYUV(im)
+  channel(im,ind) <- isoblur(channel(im,ind),sigma); 
+  YUVtoRGB(im)
+}
+#Run the function on all three channels and collect the results as a list
+blurred <- map_il(1:3,~ bchan(parrots,.))
+names(blurred) <- c("Luminance blur (Y)","Chrominance blur (U)","Chrominance blur (V)")
+plot(blurred)
+
+
+##The tidyverse package loads dplyr, purrr, etc. 
+library(tidyverse)
+library(imager)
+##Optional: cowplot has nicer defaults for ggplot
+library(cowplot) 
